491f1dc4b6679b5a0c6063b8124019bf1be208e4
Guiherme de Oliveira <guilherme.oliver12@gmail.com>
2023-10-29 12:26:42 -0300

    teste de botão de funcionário.

--- app/src/main/java/com/example/app_start/ui/funcionario/FuncionarioFragment.java
+++ app/src/main/java/com/example/app_start/ui/funcionario/FuncionarioFragment.java
@@ -1,5 +1,6 @@
 package com.example.app_start.ui.funcionario;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.app_start.R;
 import com.example.app_start.databinding.FragmentFuncionarioBinding;
+import com.google.android.material.snackbar.Snackbar;
 
-public class FuncionarioFragment extends Fragment {
+public class FuncionarioFragment extends Fragment implements View.OnClickListener {
 
     private FragmentFuncionarioBinding binding = null;
 
@@ -19,8 +22,20 @@ public class FuncionarioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        binding = FragmentFuncionarioBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return  root;
+        View view = inflater.inflate(R.layout.fragment_funcionario, container, false);
+
+        Button upButton = view.findViewById(R.id.btn_consultar_funcionario);
+        upButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_consultar_funcionario){
+            Snackbar snackbar = Snackbar.make(v, "Pressionou o botão", Snackbar.LENGTH_SHORT);
+            snackbar.setBackgroundTint(Color.WHITE);
+            snackbar.setTextColor(Color.BLACK);
+            snackbar.show();
+        }
     }
 }
