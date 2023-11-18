41bc03f060cad0ca2a0240581c8ee3130a4b9fb0
Guiherme de Oliveira <guilherme.oliver12@gmail.com>
2023-11-17 22:08:17 -0300

    criação do botão de log-out.

--- app/src/main/java/com/example/app_start/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/app_start/ui/home/HomeFragment.java
@@ -10,9 +10,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.app_start.R;
 import com.example.app_start.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private FragmentHomeBinding binding;
 
@@ -20,5 +21,20 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
